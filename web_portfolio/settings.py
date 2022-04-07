d521c8a18da4f7569e2b48ee66bdd89e6cd6748e
Weelawawt Sampao <Weelewat@gmail.com>
2022-04-07 12:49:40 +0700

    Update settings.py

--- web_portfolio/settings.py
+++ web_portfolio/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     '/var/www/static/',
