41c6f3d3b13e5a66e0b5a9b35260178565a130e8
Weelawawt Sampao <Weelewat@gmail.com>
2022-04-09 16:41:55 +0700

    Update master.css

--- web_portfolio/settings.py
+++ web_portfolio/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
